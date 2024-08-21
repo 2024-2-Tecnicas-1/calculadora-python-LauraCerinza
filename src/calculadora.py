8114cf2e81388d2916385683ab0cf8ae99f39a8f
Eddy Diaz <eddya.diazs@konradlorenz.edu.co>
2024-08-20 19:32:22 -0500

    Se agrega potencia y modulo

--- src/calculadora.py
+++ src/calculadora.py
@@ -13,6 +13,14 @@ def calcular(numero1, numero2, operacion):
         case '/':
             # Operación división
             respuesta = numero1 / numero2
+            
+        case '^':
+            # Operación potencia
+            respuesta = numero1 ** numero2
+            
+        case '%':
+            # Operación modulo
+            respuesta = numero1 % numero2
         case _:
             raise ValueError("Operación inválida.")
 
